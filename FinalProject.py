90048d919e39e42067e0b5b8b75d823a3dacc10a
andonaldson <andonaldson@csumb.edu>
2014-04-23 15:38:18 -0700

    help

--- FinalProject.py
+++ FinalProject.py
@@ -1,9 +1,11 @@
 import os
 import wx
+import wx.media
 import random
+from PIL import Image
+from array import *
 
 class MainWindow(wx.Frame):
-
     def __init__(self, parent, title):
         self.dirname=''
 
@@ -14,6 +16,7 @@ class MainWindow(wx.Frame):
 
         # Setting up the menu.
         filemenu= wx.Menu()
+        menuClear = filemenu.Append(wx.ID_CLEAR, "&Clear", "Clear the screen")
         menuOpen = filemenu.Append(wx.ID_OPEN, "&Open"," Open a file to edit")
         menuSave = filemenu.Append(wx.ID_SAVE, "&Save"," Save the Image")
         menuAbout= filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
@@ -29,24 +32,28 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnSave, menuSave)
+        self.Bind(wx.EVT_MENU, self.OnClear, menuClear)
 
         self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
         self.buttons = []
-        for i in range(0, 11):
+        for i in range(0, 12):
             self.buttons.append(wx.Button(self, -1, "Button &"+str(i)))
             self.sizer2.Add(self.buttons[i], 1, wx.EXPAND)
 
         self.buttons[0].SetLabel("Load")
         self.buttons[0].Bind(wx.EVT_BUTTON, self.ButtonClick)
         self.buttons[1].SetLabel("Blur")
-       	self.buttons[1].Bind(wx.EVT_BUTTON, self.ButtonClick2)
-       	self.buttons[2].SetLabel("Glass")
-       	self.buttons[2].Bind(wx.EVT_BUTTON, self.ButtonClick3)
-       	self.buttons[3].SetLabel("Inverse")
-       	self.buttons[3].Bind(wx.EVT_BUTTON, self.ButtonClick4)
-       	self.buttons[4].SetLabel("Solarize")
-       	self.buttons[4].Bind(wx.EVT_BUTTON, self.ButtonClick5)
-
+        self.buttons[1].Bind(wx.EVT_BUTTON, self.ButtonClick2)
+        self.buttons[2].SetLabel("Glass")
+        self.buttons[2].Bind(wx.EVT_BUTTON, self.ButtonClick3)
+        self.buttons[3].SetLabel("Inverse")
+        self.buttons[3].Bind(wx.EVT_BUTTON, self.ButtonClick4)
+        self.buttons[4].SetLabel("Solarize")
+        self.buttons[4].Bind(wx.EVT_BUTTON, self.ButtonClick5)
+        self.buttons[5].SetLabel("Posterize")
+        self.buttons[5].Bind(wx.EVT_BUTTON, self.ButtonClick6)
+        self.buttons[6].SetLabel("GreyScale")
+        self.buttons[6].Bind(wx.EVT_BUTTON, self.ButtonClick7)
         # Use some sizers to see layout options
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.sizer2, 0, wx.EXPAND)
@@ -56,42 +63,56 @@ class MainWindow(wx.Frame):
         self.SetAutoLayout(1)
         self.sizer.Fit(self)
         self.Show()
-
-    def ButtonClick(self,e):
+        
+    
+    def ButtonClick(self, e):
         png = wx.Image("C:\\Images\\leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
-                      
-    def ButtonClick2(self,e):
-        png = wx.Image("C:\\Images\\leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-    	self.myBlur(png)
-    	wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
-    	
+    
+                
+    def ButtonClick2(self, e):
+        png = wx.Image("C:\\Images\\Leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.myBlur(png)
+        wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
+    
+
     def ButtonClick3(self,e):
         png = wx.Image("C:\\Images\\leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-    	self.glassfilter(png)
-    	wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
-    	
+        self.glassfilter(png)
+        wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
+
     def ButtonClick4(self,e):
         png = wx.Image("C:\\Images\\leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-    	self.inversefilter(png)
-    	wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
+        self.inversefilter(png)
+        wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
     
     def ButtonClick5(self,e):
         png = wx.Image("C:\\Images\\leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         self.solarizefilter(png)
         wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
        
+    def ButtonClick6(self,e):
+        png = wx.Image("C:\\Images\\leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.posterizefilter(png)
+        wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))
+        
+    def ButtonClick7(self,e):
+        png = wx.Image("C:\\Images\\leaf.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.greyscalefilter(png)
+        wx.StaticBitmap(self, -1, png, (0, 0), (png.GetWidth(), png.GetHeight()))   
 
-
-
+    
     def OnAbout(self,e):
         # Create a message dialog box
         dlg = wx.MessageDialog(self, " A sample editor \n in wxPython", "About Sample Editor", wx.OK)
         dlg.ShowModal() # Shows it
         dlg.Destroy() # finally destroy it when finished.
         
-    def OnSave(self,e):
-    	self.Close(True)
+    def OnSave(self,e,):
+        #img=bmpicture.ConvertToImage()
+        #img.SaveFile("test2.png", wx.BITMAP_TYPE_PNG)
+        self.Close(True)
+        
 
     def OnExit(self,e):
         self.Close(True)  # Close the frame.
@@ -106,30 +127,107 @@ class MainWindow(wx.Frame):
             self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
+    def OnClear(self,e):
+        dc = wx.MemoryDC()
+        dc.SelectObject(wx.NullBitmap)
         
-    def solarizefilter(self, bmpicture):
-    	width= wx.Bitmap.GetWidth(bmpicture)
+        
+    
+    def greyscalefilter( self, bmpicture):
+        width= wx.Bitmap.GetWidth(bmpicture)
         height= wx.Bitmap.GetHeight(bmpicture)
         size = width,height
         dc = wx.MemoryDC()
         dc.SelectObject(bmpicture)
         hue=wx.Colour()
         oldP=wx.Colour()
-        Threshold = 5 
+        Threshold = 150
         for y in range(1, height-1):
             for x in range(1, width-1):
-            	hue=wx.DC.GetPixel(dc,x,y)
-            	newred=wx.Colour.Red(hue)
-            	newblue=wx.Colour.Blue(hue)
-            	newgreen=wx.Colour.Green(hue)
-            	oldP=wx.Colour(newred,newgreen,newblue,255)
+                hue=wx.DC.GetPixel(dc,x,y)
+                newred=wx.Colour.Red(hue)
+                newblue=wx.Colour.Blue(hue)
+                newgreen=wx.Colour.Green(hue)
+                sums= newred + newblue+ newgreen
+                averages=sums/3
+                oldP=wx.Colour(averages,averages,averages,255)
                 mypen=wx.Pen(oldP)
                 wx.Pen.SetColour(mypen,oldP)
                 wx.DC.SetPen(dc,mypen)
                 wx.DC.DrawPoint(dc,x,y)
+        dc.SelectObject(wx.NullBitmap)
+        img=bmpicture.ConvertToImage()
+        img.SaveFile("test.png", wx.BITMAP_TYPE_PNG)
+        return bmpicture
+        
+    def posterizefilter(self,bmpicture):
+        interval_values=array('i')
+        interval_count=32
+        group_size=256/interval_count
+        for i in range(0,interval_count-1):
+            interval_values.append(group_size*i)
+        
+        width= wx.Bitmap.GetWidth(bmpicture)
+        height= wx.Bitmap.GetHeight(bmpicture)
+        size = width,height
+        dc = wx.MemoryDC()
+        dc.SelectObject(bmpicture)
+        hue=wx.Colour()
+        for y in range(1, height-1):
+            for x in range(1, width-1):
+                hue=wx.DC.GetPixel(dc,x,y)
+                
+                newred=wx.Colour.Red(hue)
+                hue.Red=interval_values[int(newred/group_size)-1]
+                
+                newblue=wx.Colour.Blue(hue)
+                hue.Blue=interval_values[int(newblue/group_size)-1]
+
+                newgreen=wx.Colour.Green(hue)
+                hue.Green=interval_values[int(newgreen/group_size)-1]
+                
+                newcolor=wx.Colour(hue.Red,hue.Blue,hue.Green,255)
+                mypen=wx.Pen(newcolor)
+                wx.Pen.SetColour(mypen,newcolor)
+                wx.DC.SetPen(dc,mypen)
+                wx.DC.DrawPoint(dc,x,y)
+                
+        return bmpicture
+                
+                       
+    def solarizefilter(self, bmpicture):
+        width= wx.Bitmap.GetWidth(bmpicture)
+        height= wx.Bitmap.GetHeight(bmpicture)
+        size = width,height
+        dc = wx.MemoryDC()
+        dc.SelectObject(bmpicture)
+        hue=wx.Colour()
+        oldP=wx.Colour()
+        Threshold = 150
+        for y in range(1, height-1):
+            for x in range(1, width-1):
+                hue=wx.DC.GetPixel(dc,x,y)
+                newred=wx.Colour.Red(hue)
+                newblue=wx.Colour.Blue(hue)
+                newgreen=wx.Colour.Green(hue)
+                sums= newred + newblue+ newgreen
+                averages=sums/3
+                if averages<Threshold:
+                    newred=255-wx.Colour.Red(hue)
+                    newblue=255-wx.Colour.Blue(hue)
+                    newgreen=255-wx.Colour.Green(hue)
+                    oldP=wx.Colour(newred,newgreen,newblue,255)
+                else:                    
+                    oldP=wx.Colour(averages,255)
+                mypen=wx.Pen(oldP)
+                wx.Pen.SetColour(mypen,oldP)
+                wx.DC.SetPen(dc,mypen)
+                wx.DC.DrawPoint(dc,x,y)
+        dc.SelectObject(wx.NullBitmap)        
+        return bmpicture
         
     def inversefilter(self, bmpicture):
-    	width= wx.Bitmap.GetWidth(bmpicture)
+        width= wx.Bitmap.GetWidth(bmpicture)
         height= wx.Bitmap.GetHeight(bmpicture)
         size = width,height
         dc = wx.MemoryDC()
@@ -139,18 +237,20 @@ class MainWindow(wx.Frame):
         oldP=wx.Colour()
         for y in range(1, height-1):
             for x in range(1, width-1):
-            	hue=wx.DC.GetPixel(dc,x,y)
-            	newred=255-wx.Colour.Red(hue)
-            	newblue=255-wx.Colour.Blue(hue)
-            	newgreen=255-wx.Colour.Green(hue)
-            	oldP=wx.Colour(newred,newgreen,newblue,255)
+                hue=wx.DC.GetPixel(dc,x,y)
+                newred=255-wx.Colour.Red(hue)
+                newblue=255-wx.Colour.Blue(hue)
+                newgreen=255-wx.Colour.Green(hue)
+                oldP=wx.Colour(newred,newgreen,newblue,255)
                 mypen=wx.Pen(oldP)
                 wx.Pen.SetColour(mypen,oldP)
                 wx.DC.SetPen(dc,mypen)
                 wx.DC.DrawPoint(dc,x,y)
+        dc.SelectObject(wx.NullBitmap)        
+        return bmpicture
          
     def glassfilter(self, bmpicture):
-    	width= wx.Bitmap.GetWidth(bmpicture)
+        width= wx.Bitmap.GetWidth(bmpicture)
         height= wx.Bitmap.GetHeight(bmpicture)
         size = width,height
         Distance=5
@@ -159,19 +259,22 @@ class MainWindow(wx.Frame):
         """Goes pixel by Pixel"""
         for y in range(1, height-1):
             for x in range(1, width-1):
-            	xrand=x+random.random()*Distance*2-Distance
-            	yrand=y+random.random()*Distance*2-Distance
+                xrand=x+random.random()*Distance*2-Distance
+                yrand=y+random.random()*Distance*2-Distance
                 oldP=wx.DC.GetPixel(dc,xrand,yrand)
                 mypen=wx.Pen(oldP)
                 wx.Pen.SetColour(mypen,oldP)
                 wx.DC.SetPen(dc,mypen)
                 wx.DC.DrawPoint(dc,x,y)
+        dc.SelectObject(wx.NullBitmap)        
+        return bmpicture
                  
     def tripleSum2(self,(r1, g1, b1), (r2,g2,b2)):
         return(r1+r2,g1+g2,b1+b2)
-	
-    def myBlur(self, bmpicture):
-        """Builds and returns a new image which is a blurred copy of the argument image"""		
+
+    
+    def myBlur(self,bmpicture):
+        """Builds and returns a new image which is a blurred copy of the argument image"""    
         width= wx.Bitmap.GetWidth(bmpicture)
         height= wx.Bitmap.GetHeight(bmpicture) 
         size = width,height
@@ -192,8 +295,9 @@ class MainWindow(wx.Frame):
                 wx.DC.SetPen(dc,mypen)
                 wx.DC.DrawPoint(dc,x,y)
         dc.SelectObject(wx.NullBitmap)        
-      	return bmpicture
+        return bmpicture
+   
   
 app = wx.App(False)
-frame = MainWindow(None, "Sample editor")
+frame = MainWindow(None, "image and sound")
 app.MainLoop()
